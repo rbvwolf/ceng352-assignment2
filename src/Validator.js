cc2805ff385f36a78aafe89fb18c5888deaf8676
rbvwolf <ymysmurf27@gmail.com>
2026-05-01 23:37:23 +0300

    feat: Validator.js updated

---
+++ src/Validator.js
@@ -0,0 +1,37 @@
+class Validator {
+    // Name and surname validation
+    validateName(name) {
+        if (!name || name.trim() === "") 
+            return false;
+        const nameRegex = /^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]+$/;
+        return nameRegex.test(name);
+    }
+
+    // Email validation
+    validateEmail(email) {
+        if (!email)
+             return false;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    // Date validation
+    validateDateOfBirth(dob) {
+        if (!dob) 
+            return false;
+        const dobRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)\d\d$/;
+        return dobRegex.test(dob);
+    }
+
+    // Password validation (BVA)
+    validatePassword(password, confirmPassword) {
+        if (!password || !confirmPassword) 
+            return false;
+        if (password.length >= 8 && password === confirmPassword)
+            return true;
+        else
+            return false;
+    }
+}
+
+module.exports = Validator;
