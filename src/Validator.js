ff5077e2c76f839ccb35f7b36ba223ddc72320ad
rbvwolf <yigitmyilmaz@gmail.com>
2026-05-02 00:16:36 +0300

    pray to fix bugs

--- src/Validator.js
+++ src/Validator.js
@@ -3,8 +3,6 @@ class Validator {
     // Name and surname validation
     validateName(name) {
         if (!name || name.trim() === ""){ 
-            message.innerText = "Fill the blank space and only use letter!";
-            message.style.color = "red";
             return false;}
         const nameRegex = /^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]+$/;
         return nameRegex.test(name);
@@ -29,15 +27,11 @@ class Validator {
     // Password validation (BVA)
     validatePassword(password, confirmPassword) {
         if (!password || !confirmPassword){ 
-            message.innerText = "Error: Password must be at least 8 characters!";
-            message.style.color = "red";
             return false;}
         if (password.length >= 8 && password === confirmPassword)
             return true;
-        else{
-            message.innerText = "Error: Passwords do not match!";
-            message.style.color = "red";
-            return false;}
+        else
+            return false;
     }
 }
 
